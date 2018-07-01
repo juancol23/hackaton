a634dbc0696b0b8fe95c147b84f51da6e007e766
valdemar <juan.colichon@vasslatam.com>
2018-06-30 20:28:44 -0500

    Sprint-1 -Agregando Compatibilidad con mapas mejorado

--- app/src/main/java/weekendfever/riva/com/view/activity/detalle/Detalle.java
+++ app/src/main/java/weekendfever/riva/com/view/activity/detalle/Detalle.java
@@ -4,9 +4,15 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
-import com.google.firebase.auth.FirebaseAuth;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -17,7 +23,10 @@ import butterknife.BindView;
 import butterknife.ButterKnife;
 import weekendfever.riva.com.R;
 
-public class Detalle extends AppCompatActivity {
+public class Detalle extends AppCompatActivity implements OnMapReadyCallback {
+
+
+    private GoogleMap googleMap;
 
     @BindView(R.id.postTitleDetails)
     TextView mPostTitleDetails;
@@ -32,6 +41,7 @@ public class Detalle extends AppCompatActivity {
     private String mCategoria = null;
 
     DatabaseReference mDatabase;
+    private GoogleMap mMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,6 +50,14 @@ public class Detalle extends AppCompatActivity {
 
         ButterKnife.bind(this);
         init();
+        initMapa();
+
+    }
+
+    private void initMapa() {
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
 
     }
 
@@ -71,6 +89,14 @@ public class Detalle extends AppCompatActivity {
         });
     }
 
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        // Add a marker in Sydney and move the camera
+        LatLng sydney = new LatLng(-11.991382, -77.006932);
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    }
 }
 
 
--- app/src/main/java/weekendfever/riva/com/view/fragmentos/FragmentDiscotecas.java
+++ app/src/main/java/weekendfever/riva/com/view/fragmentos/FragmentDiscotecas.java
@@ -85,7 +85,7 @@ public class FragmentDiscotecas extends Fragment {
                         mProgress.setMessage("Accediendo...");
                         // mProgress.show();
                         //Toast.makeText(getContext(),"dale "+post_key,Toast.LENGTH_SHORT).show();
-                        Intent singleBlogIntent = new Intent(getContext(), MapsActivity.class);
+                        Intent singleBlogIntent = new Intent(getContext(), Detalle.class);
                         singleBlogIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
                         singleBlogIntent.putExtra("blog_id", post_key);
                         singleBlogIntent.putExtra("categoria", "Discotecas");
