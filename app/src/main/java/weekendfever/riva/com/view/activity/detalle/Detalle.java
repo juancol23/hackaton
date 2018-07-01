b436db6e57e682fdb35f3ee524840ec2e52890eb
valdemar <juan.colichon@vasslatam.com>
2018-06-30 20:41:14 -0500

    Sprint-1 -Agregando Compatibilidad con mapas mejorado

--- app/src/main/java/weekendfever/riva/com/view/activity/detalle/Detalle.java
+++ app/src/main/java/weekendfever/riva/com/view/activity/detalle/Detalle.java
@@ -1,7 +1,9 @@
 package weekendfever.riva.com.view.activity.detalle;
 
+import android.content.res.Resources;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -12,6 +14,7 @@ import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MapStyleOptions;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -92,6 +95,23 @@ public class Detalle extends AppCompatActivity implements OnMapReadyCallback {
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+
+        try {
+            // Customise the styling of the base map using a JSON object defined
+            // in a raw resource file.
+            boolean success = googleMap.setMapStyle(
+                    MapStyleOptions.loadRawResourceStyle(
+                            this, R.raw.mapastyleblack));
+
+            if (!success) {
+                Log.e("SYLE", "Style parsing failed.");
+                }
+            } catch (Resources.NotFoundException e) {
+                Log.e("SYLE", "Can't find style. Error: ", e);
+            }
+
+
+
         // Add a marker in Sydney and move the camera
         LatLng sydney = new LatLng(-11.991382, -77.006932);
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
